513013b09d89766edc6f35dadbed0c1e374a8990
IT22583796 <it22583796@my.sliit.lk>
2025-04-26 11:01:10 +0530

    fix some errors

--- backend/demo/src/main/java/com/learnlink/demo/notification/repository/NotificationRepository.java
+++ backend/demo/src/main/java/com/learnlink/demo/notification/repository/NotificationRepository.java
@@ -1,6 +1,4 @@
 package com.learnlink.demo.notification.repository;
-
-import com.learnlink.demo.learning.entity.Course;
 import com.learnlink.demo.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
