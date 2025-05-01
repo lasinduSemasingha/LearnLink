00eb0f52bd2432cb6e66ec77207b97670ba465a3
Kaveesha Maharambage <127282006+it22129130@users.noreply.github.com>
2025-05-01 22:02:42 +0530

    comment_dto

--- backend/demo/src/main/java/com/learnlink/demo/interaction/dto/CommentDTO.java
+++ backend/demo/src/main/java/com/learnlink/demo/interaction/dto/CommentDTO.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Builder
 public class CommentDTO {
 
-    private Long id;
+    private Long id; // Unique identifier for the comment
     private String postId;
     private String comment;
       
