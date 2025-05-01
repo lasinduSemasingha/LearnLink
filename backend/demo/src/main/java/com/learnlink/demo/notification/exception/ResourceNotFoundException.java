ebdcedf1bf4403dec1eef6824a82bda61af89366
IT22583796 <it22583796@my.sliit.lk>
2025-05-01 22:22:09 +0530

    resource message notify

--- backend/demo/src/main/java/com/learnlink/demo/notification/exception/ResourceNotFoundException.java
+++ backend/demo/src/main/java/com/learnlink/demo/notification/exception/ResourceNotFoundException.java
@@ -1,5 +1,5 @@
 package com.learnlink.demo.notification.exception;
-
+//message notify
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
         super(message);
