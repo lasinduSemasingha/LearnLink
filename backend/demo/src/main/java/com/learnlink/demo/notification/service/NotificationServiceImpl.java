7969fbefa63c821586d5864ce4a102e5317f9c92
IT22583796 <it22583796@my.sliit.lk>
2025-05-01 22:35:43 +0530

    service impl check fix import files

--- backend/demo/src/main/java/com/learnlink/demo/notification/service/NotificationServiceImpl.java
+++ backend/demo/src/main/java/com/learnlink/demo/notification/service/NotificationServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+//check fix import files
 @Service
 public class NotificationServiceImpl implements NotificationService {
 
