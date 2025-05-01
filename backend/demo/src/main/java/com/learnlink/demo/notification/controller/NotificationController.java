b7b5a950a6346848f909ce52c5c9ecfa1772bb62
IT22583796 <it22583796@my.sliit.lk>
2025-05-01 22:07:17 +0530

    notification controller update some points

--- backend/demo/src/main/java/com/learnlink/demo/notification/controller/NotificationController.java
+++ backend/demo/src/main/java/com/learnlink/demo/notification/controller/NotificationController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.management.Notification;
+//import javax.management.Notification;
 import java.util.List;
-
+//add getall method
 @RestController
 @RequestMapping("/api/notification")
 @CrossOrigin("*")
